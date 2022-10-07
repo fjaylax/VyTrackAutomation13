d0be623b55da7bd23d18b5ab70f13284e3ec08e3
Jay Alexenko <zhmenya@yahoo.com>
2022-10-07 18:40:10 -0400

    Created new VyTrack13 project

---
+++ src/test/java/com/vytrack/tests/TC9.java
@@ -0,0 +1,19 @@
+package com.vytrack.tests;
+
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TC9 {
+
+
+    @BeforeMethod
+    public void setUp() {
+
+    }
+
+    @Test
+    public static void test1(){
+
+    }
+}
---
+++ src/test/java/com/vytrack/utilities/LogIn.java
@@ -0,0 +1,18 @@
+package com.vytrack.utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+
+
+
+    public class LogIn {
+
+        WebDriver driver;
+
+        @BeforeMethod
+        public void setUp() {
+
+        }
+    }
+
