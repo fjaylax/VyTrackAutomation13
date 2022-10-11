c04ae4047b5f8d1057d46b09c7c8979f1e14cdb5
Jay Alexenko <zhmenya@yahoo.com>
2022-10-11 00:25:26 -0400

    Updated Project

--- src/test/java/com/vytrack/utilities/LogIn.java
+++ src/test/java/com/vytrack/utilities/LogIn.java
@@ -1,13 +1,9 @@
 package com.vytrack.utilities;
 
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.BeforeMethod;
-
-
-
+import org.testng.Assert;
 
 public class LogIn {
 
